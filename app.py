9275e0832a246bc04895ea870af180233295ab38
Stanulovic <76502485+Stanulovic@users.noreply.github.com>
2025-10-29 00:31:57 +0100

    Change welcome message in main route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Veljko ne zna da igra elden ring, nikola je previse dobar."
 
 @app.route('/how are you')
 def hello():
