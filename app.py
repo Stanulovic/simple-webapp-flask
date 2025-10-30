817cb073b8ab77ae57b5116e59663ed3ca645d37
Stanulovic <76502485+Stanulovic@users.noreply.github.com>
2025-10-31 00:07:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Zdravo, kako si?"
+    return """
+    <h2>Zdravo, kako si?</h2>
+    <a href="/odgovor">Idi na odgovor</a>
+    """
 
-@app.route('/odgovor')
+@app.route("/odgovor")
 def hello():
-    return 'Ja sam odlicno, kako si ti?'
+    return """
+    <h2>Ja sam odlično, kako si ti?</h2>
+    <a href="/app1/">⬅️ Nazad</a>
+    """
+
+@app.route("/app1/")
+def app1_home():
+    return """
+    <h2>Dobrodošao na /app1/ stranicu!</h2>
+    <a href="/">Idi na početnu</a>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
